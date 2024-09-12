79b87c623387e24cf6e9619ecdc932fd3acc0803
DevVictor19 <antoniovictor12@live.com>
2024-09-12 15:07:52 -0300

    feat: add concurrency on search

--- src/main/java/org/devvictor/application/services/UserService.java
+++ src/main/java/org/devvictor/application/services/UserService.java
@@ -1,6 +1,7 @@
 package org.devvictor.application.services;
 
 import org.devvictor.application.exceptions.BadRequestException;
+import org.devvictor.application.exceptions.InternalServerErrorException;
 import org.devvictor.application.exceptions.NotFoundException;
 import org.devvictor.domain.daos.UserDAO;
 import org.devvictor.domain.dtos.CreateUserRequestDTO;
@@ -9,6 +10,9 @@ import org.devvictor.domain.dtos.UpdateUserRequestDTO;
 import org.devvictor.domain.entities.User;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 public class UserService {
     private final UserDAO userDAO;
@@ -18,24 +22,35 @@ public class UserService {
     }
 
     public void create(CreateUserRequestDTO dto) {
-        var userWithSameEmail = userDAO.findByEmail(dto.email());
+        var future1 = CompletableFuture.supplyAsync(() -> userDAO.findByEmail(dto.email()));
+        var future2 = CompletableFuture.supplyAsync(() -> userDAO.findByUsername(dto.username()));
 
-        if (userWithSameEmail.isPresent()) {
-            throw new BadRequestException("Email already in use");
+        var futures = CompletableFuture.allOf(future1, future2);
+
+        try {
+            futures.join();
+
+            Optional<User> userWithSameEmail = future1.join();
+
+            if (userWithSameEmail.isPresent()) {
+                throw new BadRequestException("Email already in use");
+            }
+
+            Optional<User> userWithSameUsername = future2.join();
+
+            if (userWithSameUsername.isPresent()) {
+                throw new BadRequestException("Username already in use");
+            }
+
+            var entity = new User();
+            entity.setEmail(dto.email());
+            entity.setUsername(dto.username());
+            entity.setPassword(dto.password());
+
+            userDAO.save(entity);
+        } catch (CompletionException exception) {
+            throw new InternalServerErrorException("Cannot search user form email and username");
         }
-
-        var userWithSameUsername = userDAO.findByUsername(dto.username());
-
-        if (userWithSameUsername.isPresent()) {
-            throw new BadRequestException("Username already in use");
-        }
-
-        var entity = new User();
-        entity.setEmail(dto.email());
-        entity.setUsername(dto.username());
-        entity.setPassword(dto.password());
-
-        userDAO.save(entity);
     }
 
     public List<FindUserResponseDTO> find(int page, int itemsPerPage) {
